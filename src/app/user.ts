333899b93ddaec279300193d6e7f2af7ed7955a3
vito-corleone <cgstyle@gmail.com>
2017-03-31 12:51:27 +0200

    project init

---
+++ src/app/user.ts
@@ -0,0 +1,10 @@
+export class User {
+    constructor (
+        public Name: string,
+        public Bio: string,
+        public Location: string,
+        public Website: string
+    ) {
+        
+    }
+}
