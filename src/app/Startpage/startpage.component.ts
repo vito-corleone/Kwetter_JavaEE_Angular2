ffebf6ca5d41d868880ed60d2854647872e05a6d
vito-corleone <cgstyle@gmail.com>
2017-04-03 10:27:22 +0200

    added ajax to startpage

--- src/app/Startpage/startpage.component.ts
+++ src/app/Startpage/startpage.component.ts
@@ -1,10 +1,11 @@
-import { Component, Injectable } from '@angular/core';
+import { Component, Injectable, ChangeDetectorRef } from '@angular/core';
 import { Http } from "@angular/http";
 import 'rxjs/add/operator/map';
 import { User } from "../Domain/User/User";
 import { UserPrivate } from "../Domain/User/userPrivate";
 import { FormGroup, FormControl, Validators } from "@angular/forms";
 import { Posting } from "../Domain/Posting/Posting";
+import { Subject } from "rxjs/Subject";
 
 
 @Component({
@@ -13,34 +14,26 @@ import { Posting } from "../Domain/Posting/Posting";
 })
 @Injectable()
 export class StartpageComponent {
-
     // the user that is authenticated
     user = <User>{};
-
     // the list of the people that the user is following
     userFollowing: User[];
     // a list of users who follow this user
     userFollowedBy: User[];
     // the timeline postings, inlude postings of the user and the people the user follows
     userTimelinePostings: Posting[];
-
-    
-    userFriend = <User>{};
+    // the most recent made kweet of the user
     userRecentPost = <Posting>{};
-
-    // bidirectional value with the new Kweet field
-    postingContent: string;
-
-
-    // bidirectional value with the search field
-    searchKeyword: string;
     // List that contains all the postings that contain the searchKeyword
     searchResultPostings = <Posting>{};
 
+    search: boolean = false;
 
-    // CORS meldingen -> resources.add(CORSResponseFilter.class) verdwijnt
+    private timeLine: Subject<any> = new Subject<any>();
+    private searching: Subject<boolean> = new Subject<boolean>();
 
-    constructor(private http: Http) {
+
+    constructor(private http: Http, private _cdRef: ChangeDetectorRef) {
         this.getUserPublic();
         this.getThePeopleThatIFollow();
         this.getThePeopleThatFollowMe();
@@ -55,13 +48,13 @@ export class StartpageComponent {
             });
     }
 
-    // getUpdateUserInfo() {
-    //     this.http.get('http://localhost:8080/Kwetter/webresources/rest/getUpdateUserInfo/vito@kwetter.com/' + this.name + '/' + this.bio + '/' + this.location + '/' + this.website + '/')
-    //         .subscribe((resp) => {
-    //             console.log('getUpdateUserInfo ' + resp.json());
-    //             this.user = <User>resp.json();
-    //         });
-    // }
+    updateUserInfo(name: string, bio: string, location: string, website: string) {
+        this.http.get('http://localhost:8080/Kwetter/webresources/rest/updateUserInfo/' + this.user.emailAddress + '/' + name + '/' + bio + '/' + location + '/' + website)
+            .subscribe((resp) => {
+                console.log('saveUser ' + resp.json());
+                this.user = <User>resp.json();
+            });
+    }
 
     getThePeopleThatIFollow() {
         this.http.get('http://localhost:8080/Kwetter/webresources/rest/getThePeopleThatIFollow/vito@kwetter.com/')
@@ -79,52 +72,67 @@ export class StartpageComponent {
             });
     }
 
-
-
-
     getTimelinePostings() {
         this.http.get('http://localhost:8080/Kwetter/webresources/rest/getTimelinePostings/vito@kwetter.com/')
             .subscribe((resp) => {
                 console.log('getTimelinePostings ' + resp.json());
                 this.userTimelinePostings = resp.json();
+                // this._cdRef.markForCheck();
             });
     }
 
-    // create new Kweet
-    getCreatePost() {
-        this.http.get('http://localhost:8080/Kwetter/webresources/rest/user/posting/createPosting/1/' + this.postingContent)
-            .subscribe((resp) => {
-                console.log('getCreatePost ' + resp.json());
-                this.userRecentPost = resp.json();
-            });
-        this.postingContent = '';
-    }
 
-    // Fire a search action based on provided keyword
-    getSearchResult() {
-        this.http.get('http://localhost:8080/Kwetter/webresources/rest/getSearchResult/' + this.searchKeyword)
-            .subscribe((resp) => {
-                console.log('getSearchResult' + resp.json());
-                this.searchResultPostings = resp.json();
-            });
-        this.searchKeyword = '';
-    }
-
-
-
-    // getAllPostingsAJAX() {
-    //     this.http.get('http://localhost:8080/Kwetter/webresources/rest/posting/getAllPostings/vito@kwetter.com')
-    //     .map(res => res.json())
-    //         .subscribe((result) => this.userPostings = result);
-    // }
-
-    // onSubmit() {
-    //     this.http.get('http://localhost:8080/Kwetter/webresources/rest/getPublicUserInfo/user1@kwetter.com')
-    //         .subscribe((resp) => {
-    //             this.userFriend = <User>resp.json();
-    //             console.log(this.userFriend.location);
+    // getTimelinePostings() {
+    //     this.searching.next(true);
+    //     this.http.get('http://localhost:8080/Kwetter/webresources/rest/getTimelinePostings/vito@kwetter.com/')
+    //         .subscribe(response => {
+    //             this.timeLine.next(response.json());
+    //             console.log('getTimelinePostings' + response.json());
+    //             this.searching.next(false);
     //         });
     // }
 
+    createPost(newKweet: string) {
+        this.http.get('http://localhost:8080/Kwetter/webresources/rest/user/posting/createPosting/' + this.user.id + '/' + newKweet)
+            .subscribe((resp) => {
+                console.log('createPost ' + resp.json());
+                this.userRecentPost = resp.json();
+                this.getTimelinePostings();
+                this.search = false;
+                this._cdRef.markForCheck();
+            });
+    }
+
+    //     createPost(newKweet: string) {
+    //     this.http.get('http://localhost:8080/Kwetter/webresources/rest/user/posting/createPosting/' + this.user.id + '/' + newKweet)
+    //         .subscribe((resp) => {
+    //             console.log('createPost ' + resp.json());
+    //             this.userRecentPost = resp.json();
+    //             this.getTimelinePostings();
+    //             this._cdRef.markForCheck();
+    //         });
+    // }
+
+    // create new Kweet
+    // createPost(newKweet: string) {
+    //     this.searching.next(true);
+    //     this.http.get('http://localhost:8080/Kwetter/webresources/rest/user/posting/createPosting/' + this.user.id + '/' + newKweet)
+    //         .subscribe(response => {
+    //             this.timeLine.next(response.json());
+    //             this.searching.next(false);
+    //         });
+
+
+
+    // Fire a search action based on provided keyword
+    getSearchResult(searchKeyword: string) {
+        this.http.get('http://localhost:8080/Kwetter/webresources/rest/getSearchResult/' + searchKeyword)
+            .subscribe((resp) => {
+                console.log('getSearchResult' + resp.json());
+                this.searchResultPostings = resp.json();
+                this.search = true;
+                this._cdRef.markForCheck();
+            });
+    }
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { ProfilepageComponent}  from './Profilepage/profilepage.component';
 import { ModeratorpageComponent } from './Moderator/moderatorpage.component';
 import { routing }  from './app.routing';
 import { FormsModule }   from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
-  imports:      [ BrowserModule, routing, HttpModule, FormsModule ],
+  imports:      [ BrowserModule, routing, HttpModule, FormsModule,CommonModule ],
   declarations: [ AppComponent, StartpageComponent, ProfilepageComponent, ModeratorpageComponent ],
   bootstrap:    [ AppComponent ]
 })
